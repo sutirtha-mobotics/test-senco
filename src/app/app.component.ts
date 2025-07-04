f6bc4d3e6a8700e97b65d8a2d562e6af9ee9ff39
sutirtha <webdev1@mobotics.in>
2025-07-04 17:40:10 +0530

    change 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'demo';
+  arr = [
+    {
+      name: 'john',
+      age: 34,
+    },
+    {
+      name: 'sahil',
+      age: 24,
+    },
+    {
+      name: 'carlos',
+      age: 30,
+    },
+  ];
+
+  ageArr = JSON.stringify(this.arr.map((v) => v.age));
+  filterArr = JSON.stringify(this.arr.filter((v) => v.age > 30));
 }
